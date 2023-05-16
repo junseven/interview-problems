0ef589b7970fb6f27d45cf4b94db41a25502442a
junseven <413206188@qq.com>
2023-05-16 16:59:12 +0800

    2023/5/16

---
+++ vue/modal.vue
@@ -0,0 +1,34 @@
+<template>
+  <div v-show="showModal">
+    <button type="button"  @click="closeSelf">取消</button>
+    <button type="button"  @click="confirm">确认</button>
+  </div>
+  <button type="button" @click='createModal()'>点击我出现一个Modal</button>
+
+</template>
+ 
+<style>
+
+</style>
+ 
+<script>
+export default {
+  name: 'Modal',
+  data() {
+    return {
+      showModal:false
+    }
+  },
+  methods: {    
+    closeSelf(){
+      this.showModal = !this.showModal   
+    },
+    confirm(){
+      this.showModal = !this.showModal  
+    },
+    createModal(){
+      this.showModal = !this.showModal  
+    }
+  }
+}
+</script>
